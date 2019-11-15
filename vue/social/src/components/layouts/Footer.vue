e6ae6fe10cc6a0afda138e08d64db74dbab59bf3
Antonio Felix <antonio.felixjr@hotmail.com>
2019-11-15 20:44:20 -0300

    Create project vue

---
+++ vue/social/src/components/layouts/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer :class="'page-footer ' + cor">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">{{ logo || 'Escola' }}</h5>
+          <p class="grey-text text-lighten-4">{{ descricao ||  'Sua descrição' }}</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text">Links</h5>
+          <ul>
+           <slot></slot>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      © {{ ano || '2019' }} Copyright {{ logo || 'Escola'}}
+      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: ['cor', 'logo', 'descricao', 'ano']
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/social/src/components/layouts/Grid.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="'col s' + tamanho">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Grid',
+  props: ['tamanho'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
